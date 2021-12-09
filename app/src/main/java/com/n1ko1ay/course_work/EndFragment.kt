5792041025af650d292bc51d64c2202fa1358cdc
N1ko1ay <gastaclaus63@gmail.com>
2021-12-09 21:57:07 +0400

    EndFragment

--- app/src/main/java/com/n1ko1ay/course_work/EndFragment.kt
+++ app/src/main/java/com/n1ko1ay/course_work/EndFragment.kt
@@ -4,19 +4,23 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 
-class StartFragment : Fragment() {
+
+class EndFragment : Fragment() {
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
 
-        val view = inflater.inflate(R.layout.fragment_start, container, false)
+        val view = inflater.inflate(R.layout.fragment_end, container, false)
+
+
 
         return view
     }
+
+
 }
--- app/src/main/java/com/n1ko1ay/course_work/MainActivity.kt
+++ app/src/main/java/com/n1ko1ay/course_work/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.n1ko1ay.course_work
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var switchFragmentButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        switchFragmentButton = findViewById(R.id.switch_fragments_button)
+
+        val startFragment = StartFragment()
+        val endFragment = EndFragment()
+
+        switchFragmentButton.setOnClickListener {
+            val fragment =
+                when (supportFragmentManager.findFragmentById(R.id.fragment_container)){
+                    is StartFragment -> endFragment
+                    is EndFragment -> startFragment
+                    else -> startFragment
+                }
+
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.fragment_container, fragment)
+                .commit()
+
+        }
+
     }
 }
